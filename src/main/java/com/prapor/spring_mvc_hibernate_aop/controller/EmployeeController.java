ab77500851bf344c15c9358647b691aa14898f32
Koles1985 <orehov.1985@bk.ru>
2025-02-14 17:19:48 +0300

    stepic spring 5.3

--- src/main/java/com/prapor/spring_mvc_hibernate_aop/controller/EmployeeController.java
+++ src/main/java/com/prapor/spring_mvc_hibernate_aop/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.prapor.spring_mvc_hibernate_aop.controller;
 
 import com.prapor.spring_mvc_hibernate_aop.dao.EmployeeDAO;
 import com.prapor.spring_mvc_hibernate_aop.entity.Employee;
+import com.prapor.spring_mvc_hibernate_aop.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,12 +14,12 @@ import java.util.List;
 @Controller
 public class EmployeeController {
     @Autowired
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
     @RequestMapping("/")
     public String showAllEmployees(Model model){
 
-        List<Employee> employeeList = employeeDAO.getAllEmployees();
+        List<Employee> employeeList = employeeService.getAllEmployees();
         model.addAttribute("employeeList", employeeList);
 
 
